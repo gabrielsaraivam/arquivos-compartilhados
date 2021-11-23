ddfdfffb1fe8c39f5bb0057f294dda28dcb088d4
Gabriel Saraiva <79462440+gabrielsaraivam@users.noreply.github.com>
2021-11-23 15:25:27 -0300

    att site

--- API-projeto-site/public/funcoesLocalizacao.js
+++ API-projeto-site/public/funcoesLocalizacao.js
@@ -2,19 +2,20 @@ function getLocalizacao(){
     var id_usuario = sessionStorage.id_usuario_meuapp;
     var cargo = sessionStorage.cargo_usuario_meuapp;
     var fkEmpresa = sessionStorage.fkEmpresa_usuario_meuapp;
-    if(cargo == "Gerente"){
+    if(cargo == "Gerente" || cargo == "Analista"){
       id_usuario = fkEmpresa;
     }
     fetch(`/tblCaixaEletronicos/localizacao/${id_usuario}`, { cache: 'no-store' }).then(function (response) {
             if (response.ok) {
                 response.json().then(function (resposta) {
                     console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-                    resposta.reverse();
+                    // resposta.reverse();
                     Cookies.set(`qtdLocalizacao`, resposta.length);
    
                     for(var i = 0; i<resposta.length; i++){
                         Cookies.set(`latitudeMaq${i+1}`, `${resposta[i].latitude}`);
                         Cookies.set(`longitudeMaq${i+1}`, `${resposta[i].longitude}`);
+                        sessionStorage[`maquina${i+1}`] = resposta[i].id; 
                     }
                     // alert(resposta[0].latitude);
                     // alert(resposta[0].longitude);
